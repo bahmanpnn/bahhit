16fdabb14940d6cfe1d356fee4fa1708c7c686d4
Bahman Pournazari <bahmanpn@gmail.com>
2023-03-17 18:48:06 +0330

    7-reate post with api and set poster to add auto with request

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class PostList(ListAPIView):
+class PostList(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
